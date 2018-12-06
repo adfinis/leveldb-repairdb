5bd904c23f4d9f743d4a1add2330bc9173320240
Lukas Grossar <lukas.grossar@adfinis-sygroup.ch>
2018-12-06 09:22:58 +0100

    clang-format leveldb-repairdb.cpp

--- leveldb-repairdb.cpp
+++ leveldb-repairdb.cpp
@@ -1,22 +1,22 @@
+#include "leveldb/db.h"
 #include <iostream>
 #include <unistd.h>
-#include "leveldb/db.h"
 using namespace std;
 
 int main() {
-    leveldb::Options options;
-    char cwd[2048];
-                   
-    getcwd(cwd, sizeof(cwd));
+  leveldb::Options options;
+  char cwd[2048];
 
-	cout << "Repairing the database in the current directory." << endl;
-    leveldb::Status status = leveldb::RepairDB(cwd, options);
+  getcwd(cwd, sizeof(cwd));
 
-    if (status.ok()) {
-        cerr << "Repairing the database was sucessful." << endl;
-        return 0;
-    } else {
-        cerr << "Repairing the database failed." << endl;
-        return 1;
-    }
+  cout << "Repairing the database in the current directory." << endl;
+  leveldb::Status status = leveldb::RepairDB(cwd, options);
+
+  if (status.ok()) {
+    cerr << "Repairing the database was sucessful." << endl;
+    return 0;
+  } else {
+    cerr << "Repairing the database failed." << endl;
+    return 1;
+  }
 }
